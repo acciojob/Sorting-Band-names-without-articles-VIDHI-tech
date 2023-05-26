a38d51708b5f2f66b47cb84c2b449069f19e6239
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-26 14:43:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 //your code here
-// let arr = ["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
-// let updatedList = arr.map((element) => {
-// 	// ["The" , "Virupaksha" , "Temple"]
-// 	let words = element.split(" ");
-//     let updatedString words.reduce((prev, current, index) =>{
-// 		if(current != "The" && current != "a" && current != "an"){
-// 			return prev + " " + current;
-// 		}
-// 	    return prev;
-// 	}, "")
-		
-// 	// return updatedString;
-// }) ;
-
-// let mp = {} ; //{"Virupaksha Temple" : "The Virupaksha Temple"}
-// updatedList.forEach( (element, index) => {
-// 	mp[element] = arr[i] ;
-// });
-// //["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
-
-// updatedList.sort();
-
-// let finalAns = updatedList.map((element) => {
-// 	return mp[element];
-// })
-
-// console.log(finalAns);
-
-let arr = ["Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
-
+let arr = ["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
 let updatedList = arr.map((element) => {
-  let words = element.split(" ");
-  let updatedString = words.reduce((prev, current) => {
-    if (current !== "The" && current !== "a" && current !== "an") {
-      return prev + " " + current;
-    }
-    return prev;
-  }, "");
+	// ["The" , "Virupaksha" , "Temple"]
+	let words = element.split(" ");
+    let updatedString words.reduce((prev, current, index) =>{
+		if(current != "The" && current != "a" && current != "an"){
+			return prev + " " + current;
+		}
+	    return prev;
+	}, "")
+		
+	// return updatedString;
+}) ;
 
-  return updatedString.trim();
+let mp = {} ; //{"Virupaksha Temple" : "The Virupaksha Temple"}
+updatedList.forEach( (element, index) => {
+	mp[element] = arr[i] ;
 });
-
-let mp = {};
-
-updatedList.forEach((element, index) => {
-  mp[element] = arr[index];
-});
+//["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
 
 updatedList.sort();
 
 let finalAns = updatedList.map((element) => {
-  return mp[element];
-});
+	return mp[element];
+})
 
 console.log(finalAns);
+
+// let arr = ["Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+
+// let updatedList = arr.map((element) => {
+//   let words = element.split(" ");
+//   let updatedString = words.reduce((prev, current) => {
+//     if (current !== "The" && current !== "a" && current !== "an") {
+//       return prev + " " + current;
+//     }
+//     return prev;
+//   }, "");
+
+//   return updatedString.trim();
+// });
+
+// let mp = {};
+
+// updatedList.forEach((element, index) => {
+//   mp[element] = arr[index];
+// });
+
+// updatedList.sort();
+
+// let finalAns = updatedList.map((element) => {
+//   return mp[element];
+// });
+
+// console.log(finalAns);
 
 
