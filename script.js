4e1bda35d477a4a587f93e2deb807ed816ae6cc8
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-26 14:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,29 @@
-//your code here
-let arr = ["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
-let updatedList = arr.map((element) => {
-	// ["The" , "Virupaksha" , "Temple"]
-	let words = element.split(" ");
-    let updatedString words.reduce((prev, current, index) =>{
-		if(current != "The" && current != "a" && current != "an"){
-			return prev + " " + current;
-		}
-	    return prev;
-	}, "")
-		
-	// return updatedString;
-}) ;
+let arr = ["Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
 
-let mp = {} ; //{"Virupaksha Temple" : "The Virupaksha Temple"}
-updatedList.forEach( (element, index) => {
-	mp[element] = arr[i] ;
+let updatedList = arr.map((element) => {
+  let words = element.split(" ");
+  let updatedString = words.reduce((prev, current) => {
+    if (current !== "The" && current !== "a" && current !== "an") {
+      return prev + " " + current;
+    }
+    return prev;
+  }, "");
+
+  return updatedString.trim();
 });
-//["Virupaksha Temple" , "Victoria Memorial" , "Tajmahal"]
+
+let mp = {};
+
+updatedList.forEach((element, index) => {
+  mp[element] = arr[index];
+});
 
 updatedList.sort();
 
 let finalAns = updatedList.map((element) => {
-	return mp[element];
-})
+  return mp[element];
+});
 
 console.log(finalAns);
-
-// let arr = ["Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
-
-// let updatedList = arr.map((element) => {
-//   let words = element.split(" ");
-//   let updatedString = words.reduce((prev, current) => {
-//     if (current !== "The" && current !== "a" && current !== "an") {
-//       return prev + " " + current;
-//     }
-//     return prev;
-//   }, "");
-
-//   return updatedString.trim();
-// });
-
-// let mp = {};
-
-// updatedList.forEach((element, index) => {
-//   mp[element] = arr[index];
-// });
-
-// updatedList.sort();
-
-// let finalAns = updatedList.map((element) => {
-//   return mp[element];
-// });
-
-// console.log(finalAns);
 
 
